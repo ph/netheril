2a547f9b2e86ec66af2face76ca6cabf6e847774
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-02-13 15:53:10 -0500

    feat: boot ubuntu via cloud-hypervisor

--- Makefile
+++ Makefile
@@ -1,21 +1,48 @@
-DOCKER_BIN?=docker
-IMAGE_TAG?=earlyd
-IMAGE_NAME?=earlyd
+CACHE_DIR?=.cache
+CLOUD_INIT_FILE?=$(CACHE_DIR)/cloud-init.img
+CLOUD_IMAGE_FILE?=$(CACHE_DIR)/cloud-image.raw
 
-## run: build and run
-.PHONY: run
-run: ## - build application and docker image.
-	$(DOCKER_BIN) rm -f $(IMAGE_NAME)
-	$(DOCKER_BIN) build -t $(IMAGE_TAG) . && $(DOCKER_BIN) run -it --name $(IMAGE_NAME)  $(IMAGE_TAG)
 
-## zombie: build and run
-.PHONY: zombie
-zombie: ## - build the zombie-maker
-	gcc bin/zombie-maker.c -o bin/zombie-maker
+## clean: Clean the cache directory
+clean: clean_cache ## - clean the cache directory
 
-## help : Show this help.
+## clean_cache: Clean the cache directory.
+clean_cache: ## - clean the cache directory
+	rm -rf $(CACHE_DIR)
+
+$(CLOUD_INIT_FILE): 
+	mkdir -p $(CACHE_DIR)
+	rm -f $(CLOUD_INIT_FILE)
+	mkdosfs -n CIDATA -C $(CLOUD_INIT_FILE) 8192
+	mcopy -oi $(CLOUD_INIT_FILE) -s test/data/cloud-init/user-data ::
+	mcopy -oi $(CLOUD_INIT_FILE) -s test/data/cloud-init/meta-data ::
+	mcopy -oi $(CLOUD_INIT_FILE) -s test/data/cloud-init/network-config ::
+
+$(CLOUD_IMAGE_FILE):
+	mkdir -p $(CACHE_DIR)
+	cp $(CLOUD_IMAGE) $(CLOUD_IMAGE_FILE)
+
+## vm: Build the test vm
+vm: $(CLOUD_INIT_FILE) $(CLOUD_IMAGE_FILE) ## - start a vm
+	@echo "Starting a VM"
+	cloud-hypervisor \
+	    --kernel $(HYPERVISOR_FIRMWARE) \
+	    --disk path=$(CLOUD_IMAGE_FILE) path=$(CLOUD_INIT_FILE) \
+	    --cpus boot=4 \
+	    --memory size=1024M \
+	    --net "tap=,mac=,ip=,mask=" \
+	    --serial tty \
+	    --console off
+
+## help: Show this help.
+.PHONY: help
 help: Makefile
 	@printf "Usage: make [target] [VARIABLE=value]\nTargets:\n"
 	@sed -n 's/^## //p' $< | awk 'BEGIN {FS = ":"}; { if(NF>1 && $$2!="") printf "  \033[36m%-25s\033[0m %s\n", $$1, $$2 ; else printf "%40s\n", $$1};'
 	@printf "Variables:\n"
 	@grep -E "^[A-Za-z0-9_]*\?=" $< | awk 'BEGIN {FS = "\\?="}; { printf "  \033[36m%-25s\033[0m  Default values: %s\n", $$1, $$2}'
+
+
+# ifeq (,$(wildcard $(CLOUD_IMAGE_FILE))
+# 	cp $CLOUD_IMAGE $(CLOUD_IMAGE_FILE)
+# endif
--- flake.nix
+++ flake.nix
@@ -8,26 +8,96 @@
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    # TODO: this only need to support amd64 for now.
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
+
         pkgs = import nixpkgs {
           inherit system overlays;
         };
 
-	rustVersion = pkgs.rust-bin.nightly.latest.default;
+        cloud-image =
+          let
+            inherit (pkgs) stdenv lib;
+          in
+            stdenv.mkDerivation  rec {
+              version = "2025110211";
+              name = "ubuntu-noble-server-cloudimg";
+
+              src = builtins.fetchurl {
+                url = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img";
+                sha256 = "sha256:00vji71fsgg17qsc1hxknrfh5dm7p3rspskbbgsw5wf26lj37gdm";
+              };
+
+              sourceRoot = ".";
+              buildInputs = [
+                pkgs.qemu
+              ];
+              phases = ["unpackPhase" "buildPhase" "installPhase" "configurePhase"];
+              configurePhase = ''
+                export TMP=$(mktemp -d)
+                '';
+              unpackPhase = ''
+                mkdir -p $out
+                '';
+              buildPhase = ''
+                qemu-img convert -p -f qcow2 -O raw $src $TMP/${name}.raw
+'';
+              installPhase = ''
+                cp $TMP/${name}.raw $out
+                '';
+              meta = with nixpkgs.lib; {
+                homepage = "https://cloud-images.ubuntu.com/noble";
+                description = "Ubuntu Noble Server Cloud Image";
+                platforms = platforms.linux;
+              };
+            };
+
+        cloud-hypervisor-firmware =
+          let
+            inherit (pkgs) stdenv lib;
+          in
+            stdenv.mkDerivation  rec {
+              version = "0.5.0";
+              name = "cloud-hypervisor-firmware";
+              src = builtins.fetchurl {
+                url = "https://github.com/cloud-hypervisor/rust-hypervisor-firmware/releases/download/${version}/hypervisor-fw";
+                sha256 = "sha256:0n6rdb0kwdwmwqlpxq2sbszm18zrxnyid8lq45fv3xk8ffbiw2ja";
+              };
+
+              sourceRoot = ".";
+              phases = ["unpackPhase" "installPhase"];
+              unpackPhase = ''
+                mkdir -p $out
+                '';
+              installPhase = ''
+                cp $src $out/${name}
+                '';
+              meta = with nixpkgs.lib; {
+                homepage = "https://github.com/cloud-hypervisor/rust-hypervisor-firmware";
+                description = "Hypervisor firmware for cloud-hypervisor";
+                platforms = platforms.linux;
+              };
+            };
+
+        rustVersion = pkgs.rust-bin.nightly.latest.default;
       in {
         devShell = pkgs.mkShell {
           buildInputs =
             [
               (rustVersion.override { extensions = [ "rust-src" "rustfmt" "clippy" ]; })
               pkgs.rust-analyzer
-              pkgs.openssl
-              pkgs.glibc
-              pkgs.pkg-config
               pkgs.cmake
-              pkgs.act
+              pkgs.cloud-hypervisor
+              pkgs.dosfstools
+              pkgs.mtools
+              cloud-image
+              cloud-hypervisor-firmware
             ];
+
+          CLOUD_IMAGE="${cloud-image}/${cloud-image.name}.raw";
+          HYPERVISOR_FIRMWARE="${cloud-hypervisor-firmware}/${cloud-hypervisor-firmware.name}";
         };
       });
 }
