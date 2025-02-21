59b48d8c9e6379b7ea96b92ade02337304e69a7b
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-02-21 13:52:18 -0500

    feat: return build information on root "/" (#5)
    * feat: return build information on root "/"

---
+++ build.rs
@@ -0,0 +1,17 @@
+use vergen_git2::{BuildBuilder, Emitter, Git2Builder};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    build_informations()
+}
+
+fn build_informations() -> Result<(), Box<dyn std::error::Error>> {
+    let build = BuildBuilder::all_build()?;
+    let git2 = Git2Builder::all_git()?;
+
+    Emitter::default()
+        .add_instructions(&build)?
+        .add_instructions(&git2)?
+        .emit()?;
+
+    Ok(())
+}
