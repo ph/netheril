0dc87cc5e08d18c5a46b34805ec9101b9acc5281
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-02-25 12:58:26 -0500

    add unit test for api

--- src/app.rs
+++ src/app.rs
@@ -23,7 +23,7 @@ impl App {
     pub async fn run(&self) -> Result<(), Box<NetherilErr>> {
         info!("starting");
 
-        let router = self.router();
+        let router = router();
 
         let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
             .await
@@ -35,27 +35,27 @@ impl App {
 
         Ok(())
     }
+}
 
-    pub fn router(&self) -> Router {
-        //.merge(self.swagger_ui())
-        Router::new()
-            .merge(self.swagger_ui())
-            .nest("/api/", root::router())
-    }
+fn router() -> Router {
+    //.merge(self.swagger_ui())
+    Router::new()
+	.merge(swagger_ui())
+	.nest("/api/", root::router())
+}
 
-    pub fn swagger_ui(&self) -> SwaggerUi {
-        #[derive(OpenApi)]
-        #[openapi(
-	    nest(
-		(path = "/api", api = root::ApiDoc)
-	    )
-	)]
-        struct ApiDoc;
+pub fn swagger_ui() -> SwaggerUi {
+    #[derive(OpenApi)]
+    #[openapi(
+	nest(
+	    (path = "/api", api = root::ApiDoc)
+	)
+    )]
+    struct ApiDoc;
 
-        let doc = ApiDoc::openapi();
+    let doc = ApiDoc::openapi();
 
-        SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", doc)
-    }
+    SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", doc)
 }
 
 impl Default for App {
@@ -66,7 +66,7 @@ impl Default for App {
 
 mod root {
     use axum::{http::StatusCode, routing::get, Json, Router};
-    use serde::Serialize;
+    use serde::{Serialize};
     use utoipa::{OpenApi, ToSchema};
 
     use crate::version::{self, Build};
@@ -80,7 +80,7 @@ mod root {
     }
 
     #[derive(Serialize, ToSchema)]
-    struct RootResponse {
+    pub struct RootResponse {
         message: &'static str,
         build: Build,
     }
@@ -103,5 +103,120 @@ mod root {
     )]
     async fn index() -> (StatusCode, Json<RootResponse>) {
         (StatusCode::OK, Json(RootResponse::default()))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::net::SocketAddr;
+    use reqwest::RequestBuilder;
+    use serde::Deserialize;
+    use tokio::task::JoinHandle;
+    use crate::version::BUILD;
+
+    use super::*;
+
+    #[derive(Debug)]
+    struct Client{
+	addr: SocketAddr,
+	client: reqwest::Client,
+    }
+    
+    impl Client {
+	fn new(addr: SocketAddr) -> Result<Self, Box<dyn std::error::Error>> {
+	    let client = reqwest::Client::builder().build()?;
+	    Ok(Self { addr, client })
+	}
+
+	fn get<R: Into<RelativeUrl>>(&self, path: R) -> RequestBuilder {
+	    let url = self.base_url(path.try_into().unwrap());
+	    self.client.get(url) 
+	}
+
+	fn base_url(&self, path: RelativeUrl) -> String {
+	    format!("http://{}:{}{}", self.addr.ip(), self.addr.port(), path)
+	}
+    }
+
+    #[derive(Debug)]
+    struct RelativeUrl(String);
+
+    impl std::fmt::Display for RelativeUrl {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+	    write!(f, "{}", self.0)
+        }
+    }
+
+    impl From<&str> for RelativeUrl {
+        fn from(value: &str) -> Self {
+	    const HTTP_PREFIX: &str = "http://";
+	    const HTTPS_PREFIX: &str = "https://";
+
+	    let candidate = value.to_lowercase();
+
+	    if candidate.starts_with(HTTP_PREFIX) && candidate.starts_with(HTTPS_PREFIX) {
+		panic!("bad relative url: `{}`", value)
+	    }
+
+	    RelativeUrl(value.to_string())
+        }
+    }
+
+    #[derive(Debug)]
+    struct TestServer {
+	server_handle: JoinHandle<()>,
+    }
+
+    impl TestServer {
+	async fn new(router: Router) -> Result<(Self, Client), Box<dyn std::error::Error>> {
+	    const ANY_LOCAL_PORT: &str = "0.0.0.0:0";
+
+	    let listener = tokio::net::TcpListener::bind(ANY_LOCAL_PORT).await?;
+	    let addr = listener.local_addr()?;
+
+	    let server_handle = tokio::spawn(async move {
+		axum::serve(listener, router)
+		    .await
+		    .unwrap();
+	    });
+
+	    Ok((Self { server_handle }, Client::new(addr)?))
+	}
+    }
+
+    impl Drop for TestServer {
+        fn drop(&mut self) {
+	    self.server_handle.abort()
+        }
+    }
+
+    #[tokio::test]
+    async fn it_should_return_the_build_information() {
+	#[derive(Deserialize)]
+	struct BuildResponse {
+	    version: String,
+	    git_sha: String,
+	    build_date: String,
+	}
+	
+	#[derive(Deserialize)]
+	struct Response {
+	    message: String,
+	    build: BuildResponse,
+	}
+
+	let (_server, client) = TestServer::new(router()).await.unwrap();
+
+	let response: Response = client.get("/api/")
+	    .send()
+	    .await.unwrap()
+	    .json()
+	    .await.unwrap();
+
+	assert_eq!(response.message, "Hello from Netheril");
+
+	assert_eq!(response.build.version, BUILD.version);
+	assert_eq!(response.build.git_sha, BUILD.git_sha);
+	assert_eq!(response.build.build_date, BUILD.build_date);
     }
 }
