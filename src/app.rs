31fa04a12c3804e6df30f8cdfd9fbfcc3ffccd0d
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-02-26 10:16:11 -0500

    feat: handle ctrl-c (#13)
    * feat: handle ctrl-c
    
    double ctrl-c will send the appropriate message to axum.

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,5 @@
 use axum::Router;
+use tokio::sync::broadcast::{self, Receiver, Sender};
 use tracing::info;
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
@@ -11,6 +12,21 @@ use crate::{
 pub struct App {
     #[allow(dead_code)]
     logging: Logging,
+}
+
+#[derive(Debug, Clone)]
+enum Broadcast {
+    Interrupt,
+}
+
+impl std::fmt::Display for Broadcast {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Broadcast::*;
+
+        match self {
+            Interrupt => write!(f, "broadcast interrupt"),
+        }
+    }
 }
 
 impl App {
@@ -29,11 +45,52 @@ impl App {
             .await
             .map_err(|e| NetherilErr::Api(e.to_string()))?;
 
-        axum::serve(listener, router)
-            .await
-            .map_err(|e| NetherilErr::Api(e.to_string()))?;
+        let (broadcast, rx) = broadcast::channel::<Broadcast>(1);
+
+        let mut handles = Vec::new();
+
+        let handle = tokio::spawn(async move {
+            let _ = register_signals(broadcast).await;
+        });
+        handles.push(handle);
+
+        let handle = tokio::spawn(async move {
+            let rx = rx.resubscribe();
+
+            axum::serve(listener, router)
+                .with_graceful_shutdown(handle_shutdown_signal(rx))
+                .await
+                .map_err(|e| NetherilErr::Api(e.to_string()))
+                .unwrap();
+        });
+        handles.push(handle);
+
+        for handle in handles {
+            handle.await.unwrap();
+        }
 
         Ok(())
+    }
+}
+
+async fn handle_shutdown_signal(mut receiver: Receiver<Broadcast>) {
+    match receiver.recv().await {
+        Ok(Broadcast::Interrupt) | Err(_) => (),
+    }
+}
+
+async fn register_signals(broadcast: Sender<Broadcast>) -> Result<(), Box<dyn std::error::Error>> {
+    let mut interrupt_count = 0;
+    loop {
+        tokio::select! {
+            _ = tokio::signal::ctrl_c() => {
+            interrupt_count += 1;
+            if interrupt_count > 1 {
+                broadcast.send(Broadcast::Interrupt)?;
+                return Ok(())
+            }
+            }
+        }
     }
 }
 
