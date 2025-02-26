cd27fad14aa864404d388e6d7b25f45e92100508
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-02-26 13:45:00 -0500

    Feature/create layout initial logic for operations (#15)
    - Add operations/{id} endpoint
    - use uppercase and snake_case as enum values
    - skeleton of OperationService

--- src/app.rs
+++ src/app.rs
@@ -96,16 +96,18 @@ async fn register_signals(broadcast: Sender<Broadcast>) -> Result<(), Box<dyn st
 
 fn router() -> Router {
     //.merge(self.swagger_ui())
-    Router::new()
-        .merge(swagger_ui())
-        .nest("/api/", root::router())
+    Router::new().merge(swagger_ui()).nest(
+        "/api/",
+        root::router().nest("/operations/", operations::router()),
+    )
 }
 
 pub fn swagger_ui() -> SwaggerUi {
     #[derive(OpenApi)]
     #[openapi(
 	nest(
-	    (path = "/api", api = root::ApiDoc)
+	    (path = "/api", api = root::ApiDoc),
+	    (path = "/api/operations/", api = operations::ApiDoc),
 	)
     )]
     struct ApiDoc;
@@ -118,6 +120,71 @@ pub fn swagger_ui() -> SwaggerUi {
 impl Default for App {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[allow(unused)]
+struct OperationService {}
+
+#[allow(unused)]
+impl OperationService {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn find(&self, id: &str) {
+        println!("find: {}", id);
+    }
+}
+
+mod operations {
+    use axum::{extract::Path, http::StatusCode, routing::get, Json, Router};
+    use serde::{Deserialize, Serialize};
+    use utoipa::{OpenApi, ToSchema};
+
+    #[derive(OpenApi)]
+    #[openapi(paths(show))]
+    pub struct ApiDoc;
+
+    pub fn router() -> Router {
+        Router::new().route("/{id}", get(show))
+    }
+
+    #[derive(Debug, Deserialize)]
+    struct ShowPath {
+        id: String,
+    }
+
+    #[derive(Debug, Serialize, ToSchema)]
+    #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+    enum Status {
+        // Completed,
+        // Error,
+        // Queued,
+        InProgress,
+    }
+
+    #[derive(Debug, Serialize, ToSchema)]
+    struct ShowResponse {
+        operation_id: String,
+        status: Status,
+    }
+
+    #[utoipa::path(
+	get,
+	path = "/operations/:id",
+	responses(
+	    (status = OK, description = "Successfully retrieve the specified operation", body = ShowResponse)
+	)
+    )]
+    async fn show(Path(ShowPath { id }): Path<ShowPath>) -> (StatusCode, Json<ShowResponse>) {
+        (
+            StatusCode::OK,
+            Json(ShowResponse {
+                operation_id: id,
+                status: Status::InProgress,
+            }),
+        )
     }
 }
 
