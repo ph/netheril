fdb387288263daaba1ae1fc559fe92af682540d1
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-03-13 09:08:50 -0400

    fix: remove cloud image, since its not used yet and speed up the test

--- flake.nix
+++ flake.nix
@@ -17,70 +17,6 @@
           inherit system overlays;
         };
 
-        cloud-image =
-          let
-            inherit (pkgs) stdenv lib;
-          in
-            stdenv.mkDerivation  rec {
-              version = "0250218.1";
-              name = "ubuntu-noble-server-cloudimg";
-
-              src = builtins.fetchurl {
-                url = "https://cloud-images.ubuntu.com/focal/0250218.1/focal-server-cloudimg-amd64.img";
-                sha256 = "sha256:00vji71fsgg17qsc1hxknrfh5dm7p3rspskbbgsw5wf26lj37gdm";
-              };
-
-              sourceRoot = ".";
-              buildInputs = [
-                pkgs.qemu
-              ];
-              phases = ["unpackPhase" "buildPhase" "installPhase" "configurePhase"];
-              configurePhase = ''
-                export TMP=$(mktemp -d)
-                '';
-              unpackPhase = ''
-                mkdir -p $out
-                '';
-              buildPhase = ''
-                qemu-img convert -p -f qcow2 -O raw $src $TMP/${name}.raw
-'';
-              installPhase = ''
-                cp $TMP/${name}.raw $out
-                '';
-              meta = with nixpkgs.lib; {
-                homepage = "https://cloud-images.ubuntu.com/noble";
-                description = "Ubuntu Noble Server Cloud Image";
-                platforms = platforms.linux;
-              };
-            };
-
-        cloud-hypervisor-firmware =
-          let
-            inherit (pkgs) stdenv lib;
-          in
-            stdenv.mkDerivation  rec {
-              version = "0.5.0";
-              name = "cloud-hypervisor-firmware";
-              src = builtins.fetchurl {
-                url = "https://github.com/cloud-hypervisor/rust-hypervisor-firmware/releases/download/${version}/hypervisor-fw";
-                sha256 = "sha256:0n6rdb0kwdwmwqlpxq2sbszm18zrxnyid8lq45fv3xk8ffbiw2ja";
-              };
-
-              sourceRoot = ".";
-              phases = ["unpackPhase" "installPhase"];
-              unpackPhase = ''
-                mkdir -p $out
-                '';
-              installPhase = ''
-                cp $src $out/${name}
-                '';
-              meta = with nixpkgs.lib; {
-                homepage = "https://github.com/cloud-hypervisor/rust-hypervisor-firmware";
-                description = "Hypervisor firmware for cloud-hypervisor";
-                platforms = platforms.linux;
-              };
-            };
-
         rustVersion = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       in {
         devShell = pkgs.mkShell {
@@ -94,12 +30,7 @@
               pkgs.mtools
               pkgs.gh
               pkgs.act
-              cloud-image
-              cloud-hypervisor-firmware
             ];
-
-          CLOUD_IMAGE="${cloud-image}/${cloud-image.name}.raw";
-          HYPERVISOR_FIRMWARE="${cloud-hypervisor-firmware}/${cloud-hypervisor-firmware.name}";
         };
       });
 }
