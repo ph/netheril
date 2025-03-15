268c7796fabd5f7a00fc39e5a12a2995b7639cc0
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-03-14 20:28:18 -0400

    run `make fix`

--- src/operation/mod.rs
+++ src/operation/mod.rs
@@ -3,7 +3,7 @@ use std::collections::BTreeMap;
 
 use async_trait::async_trait;
 use error::OperationError;
-use operation_model::{Operation};
+use operation_model::Operation;
 use sentinel::Sentinel;
 use states::State;
 use tokio::sync::oneshot;
@@ -11,9 +11,9 @@ use tokio::sync::oneshot;
 use crate::actor::{Actor, ActorError, Context};
 
 mod error;
+mod operation_model;
 mod sentinel;
 mod states;
-mod operation_model;
 
 #[derive(Debug, Clone, Copy, PartialEq, Ord, PartialOrd, Eq)]
 pub struct Id(uuid::Uuid);
@@ -41,7 +41,7 @@ impl OperationStateManagerActor {
     pub fn new(receiver: tokio::sync::mpsc::Receiver<Message>) -> Self {
         OperationStateManagerActor {
             operations: BTreeMap::new(),
-	    receiver,
+            receiver,
         }
     }
 }
@@ -49,9 +49,18 @@ impl OperationStateManagerActor {
 #[derive(Debug)]
 enum Message {
     Quit,
-    LookupOperation { id: Id, reply_to: oneshot::Sender<Option<Operation>> },
-    NewOperation { reply_to: oneshot::Sender<Id> },
-    UpdateOperation { id: Id, from: State, to: State },
+    LookupOperation {
+        id: Id,
+        reply_to: oneshot::Sender<Option<Operation>>,
+    },
+    NewOperation {
+        reply_to: oneshot::Sender<Id>,
+    },
+    UpdateOperation {
+        id: Id,
+        from: State,
+        to: State,
+    },
 }
 
 #[async_trait]
@@ -59,28 +68,27 @@ impl Actor for OperationStateManagerActor {
     type Message = Message;
 
     async fn handle(&mut self, _ctx: &Context, message: Self::Message) -> Result<(), ActorError> {
-	use Message::*;
+        use Message::*;
 
-
-	match message {
-	    NewOperation { reply_to } =>  {
-		let operation = Operation::new();
-		let id = operation.id().clone();
-		self.operations.insert(id.clone(), operation);
-		reply_to.send(id.clone());
-	    },
-	    LookupOperation { id, reply_to } => {
-		let operation = self.operations.get(&id).cloned();
-		reply_to.send(operation);
-	    }
-	    UpdateOperation { id, from, to} => {
-		if let Some(operation) = self.operations.get_mut(&id) {
-		    operation.apply(from, to);
-		}
-	    }
-	    Quit => { }
-	}
-	Ok(())
+        match message {
+            NewOperation { reply_to } => {
+                let operation = Operation::new();
+                let id = operation.id().clone();
+                self.operations.insert(id.clone(), operation);
+                reply_to.send(id.clone());
+            }
+            LookupOperation { id, reply_to } => {
+                let operation = self.operations.get(&id).cloned();
+                reply_to.send(operation);
+            }
+            UpdateOperation { id, from, to } => {
+                if let Some(operation) = self.operations.get_mut(&id) {
+                    operation.apply(from, to);
+                }
+            }
+            Quit => {}
+        }
+        Ok(())
     }
 }
 
@@ -90,41 +98,50 @@ struct OperationStateManagerHandle {
 
 impl OperationStateManagerHandle {
     pub fn new() -> Self {
-	let (sender, receiver) = tokio::sync::mpsc::channel(OPERATION_STATE_MANAGER_CAPACITY);
-	let manager = OperationStateManagerActor::new(receiver);
-	let handle = OperationStateManagerHandle {
-	    sender,
-	};
+        let (sender, receiver) = tokio::sync::mpsc::channel(OPERATION_STATE_MANAGER_CAPACITY);
+        let manager = OperationStateManagerActor::new(receiver);
+        let handle = OperationStateManagerHandle { sender };
 
-	tokio::spawn(execute_operation_state_manager(manager));
+        tokio::spawn(execute_operation_state_manager(manager));
 
-	handle
+        handle
     }
 
     pub async fn new_operation(&self) -> Result<Id, OperationError> {
-	let (tx, rx) = oneshot::channel();
-	self.sender.send(Message::NewOperation { reply_to: tx }).await?;
-	Ok(rx.await?)
+        let (tx, rx) = oneshot::channel();
+        self.sender
+            .send(Message::NewOperation { reply_to: tx })
+            .await?;
+        Ok(rx.await?)
     }
 
     pub async fn lookup_operation(&self, id: &Id) -> Result<Option<Operation>, OperationError> {
-	let (tx, rx) = oneshot::channel();
-	self.sender.send(Message::LookupOperation { id: id.clone(), reply_to: tx }).await?;
-	Ok(rx.await?)
+        let (tx, rx) = oneshot::channel();
+        self.sender
+            .send(Message::LookupOperation {
+                id: id.clone(),
+                reply_to: tx,
+            })
+            .await?;
+        Ok(rx.await?)
     }
 
     pub async fn new_sentinel(&self, id: Id) -> Result<Sentinel, OperationError> {
-	match self.lookup_operation(&id).await? {
-	    Some(operation) => Ok(Sentinel::reify(id.clone(), operation.state(), self.sender.clone())),
-	    None => Err(OperationError::NotFound(id)),
-	}
+        match self.lookup_operation(&id).await? {
+            Some(operation) => Ok(Sentinel::reify(
+                id.clone(),
+                operation.state(),
+                self.sender.clone(),
+            )),
+            None => Err(OperationError::NotFound(id)),
+        }
     }
 }
 
 async fn execute_operation_state_manager(mut manager: OperationStateManagerActor) {
     let ctx = Context::new();
     while let Some(message) = manager.receiver.recv().await {
-	manager.handle(&ctx, message).await.unwrap();
+        manager.handle(&ctx, message).await.unwrap();
     }
 }
 
@@ -134,45 +151,48 @@ mod test {
 
     #[tokio::test]
     async fn create_new_operation() {
-	let op_state = OperationStateManagerHandle::new();
-	let id = op_state.new_operation().await.unwrap();
+        let op_state = OperationStateManagerHandle::new();
+        let id = op_state.new_operation().await.unwrap();
     }
 
     #[tokio::test]
     async fn return_true_when_operation_exists() {
-	let op_state = OperationStateManagerHandle::new();
-	let id = op_state.new_operation().await.unwrap();
-	let operation = op_state.lookup_operation(&id).await.unwrap().unwrap();
-	assert_eq!(operation.id(), id);
+        let op_state = OperationStateManagerHandle::new();
+        let id = op_state.new_operation().await.unwrap();
+        let operation = op_state.lookup_operation(&id).await.unwrap().unwrap();
+        assert_eq!(operation.id(), id);
     }
 
     #[tokio::test]
     async fn cant_create_sentinel_when_operation_does_not_exist() {
-	let op_state = OperationStateManagerHandle::new();
-	let id = Id::generate();
-	assert!(matches!(op_state.new_sentinel(id.clone()).await, Err(OperationError::NotFound(id))))
+        let op_state = OperationStateManagerHandle::new();
+        let id = Id::generate();
+        assert!(matches!(
+            op_state.new_sentinel(id.clone()).await,
+            Err(OperationError::NotFound(id))
+        ))
     }
 
     #[tokio::test]
     async fn create_sentinel_when_operation_exists() {
-	let op_state = OperationStateManagerHandle::new();
-	let id = op_state.new_operation().await.unwrap();
-	let sentinel = op_state.new_sentinel(id).await.unwrap();
-	assert_eq!(sentinel.id(), id);
+        let op_state = OperationStateManagerHandle::new();
+        let id = op_state.new_operation().await.unwrap();
+        let sentinel = op_state.new_sentinel(id).await.unwrap();
+        assert_eq!(sentinel.id(), id);
     }
 
     #[tokio::test]
     async fn update_operation_state_from_sentinel() {
-	let op_state = OperationStateManagerHandle::new();
-	let id = op_state.new_operation().await.unwrap();
-	let mut sentinel = op_state.new_sentinel(id.clone()).await.unwrap();
+        let op_state = OperationStateManagerHandle::new();
+        let id = op_state.new_operation().await.unwrap();
+        let mut sentinel = op_state.new_sentinel(id.clone()).await.unwrap();
 
-	sentinel.start().await.unwrap();
-	let operation = op_state.lookup_operation(&id).await.unwrap().unwrap();
-	assert_eq!(operation.state(), State::Working);
+        sentinel.start().await.unwrap();
+        let operation = op_state.lookup_operation(&id).await.unwrap().unwrap();
+        assert_eq!(operation.state(), State::Working);
 
-	sentinel.complete().await.unwrap();
-	let operation = op_state.lookup_operation(&id).await.unwrap().unwrap();
-	assert_eq!(operation.state(), State::Completed);
+        sentinel.complete().await.unwrap();
+        let operation = op_state.lookup_operation(&id).await.unwrap().unwrap();
+        assert_eq!(operation.state(), State::Completed);
     }
 }
--- src/operation/sentinel.rs
+++ src/operation/sentinel.rs
@@ -11,15 +11,11 @@ pub struct Sentinel {
 
 impl Sentinel {
     pub fn new(id: Id, sender: tokio::sync::mpsc::Sender<Message>) -> Self {
-	Self::reify(id, State::Queued, sender)
+        Self::reify(id, State::Queued, sender)
     }
 
     pub fn reify(id: Id, state: State, sender: tokio::sync::mpsc::Sender<Message>) -> Self {
-	Sentinel {
-	    id,
-	    state,
-	    sender,
-	}
+        Sentinel { id, state, sender }
     }
 
     pub fn id(&self) -> Id {
@@ -31,56 +27,56 @@ impl Sentinel {
     }
 
     pub async fn start(&mut self) -> Result<(), OperationError> {
-	self.apply(State::Working).await
+        self.apply(State::Working).await
     }
 
     pub async fn fail(&mut self, _error: OperationError) -> Result<(), OperationError> {
-	self.apply(State::Failed).await
+        self.apply(State::Failed).await
     }
 
     pub async fn cancel(&mut self) -> Result<(), OperationError> {
-	self.apply(State::Canceled).await
-
+        self.apply(State::Canceled).await
     }
 
     pub async fn complete(&mut self) -> Result<(), OperationError> {
-	self.apply(State::Completed).await
+        self.apply(State::Completed).await
     }
 
     async fn transition(&mut self, new_state: State) -> Result<(), OperationError> {
         let from = self.state.clone();
         let to = new_state.clone();
 
-	self.communicate_changes(from, to).await?;
+        self.communicate_changes(from, to).await?;
         self.state = new_state;
 
         Ok(())
     }
 
     async fn communicate_changes(&self, from: State, to: State) -> Result<(), OperationError> {
-	let message = Message::UpdateOperation {
-	    id: self.id.clone(),
-	    from, to,
-	};
+        let message = Message::UpdateOperation {
+            id: self.id.clone(),
+            from,
+            to,
+        };
 
-	self.sender.send(message).await?;
-	Ok(())
+        self.sender.send(message).await?;
+        Ok(())
     }
 
     async fn apply(&mut self, new_state: State) -> Result<(), OperationError> {
-	match (self.state.clone(), new_state.clone()) {
-	    (State::Queued, State::Working) => self.transition(new_state).await,
-	    (State::Queued, State::Canceled) => self.transition(new_state).await,
+        match (self.state.clone(), new_state.clone()) {
+            (State::Queued, State::Working) => self.transition(new_state).await,
+            (State::Queued, State::Canceled) => self.transition(new_state).await,
 
-	    // transition to terminal states.
-	    (State::Working, State::Failed) => self.transition(new_state).await,
-	    (State::Working, State::Canceled) => self.transition(new_state).await,
-	    (State::Working, State::Completed) => self.transition(new_state).await,
-	    _ => Err(OperationError::InvalidTransition {
-		from: self.state.clone(),
-		to: new_state,
-	    }),
-	}
+            // transition to terminal states.
+            (State::Working, State::Failed) => self.transition(new_state).await,
+            (State::Working, State::Canceled) => self.transition(new_state).await,
+            (State::Working, State::Completed) => self.transition(new_state).await,
+            _ => Err(OperationError::InvalidTransition {
+                from: self.state.clone(),
+                to: new_state,
+            }),
+        }
     }
 }
 
@@ -91,178 +87,202 @@ mod test {
     use super::*;
 
     fn sentinel() -> (Id, Receiver<Message>, Sentinel) {
-	let (tx, rx) = tokio::sync::mpsc::channel(1);
-	let id = Id::generate();
-	let sentinel = Sentinel::new(id.clone(), tx);
-	(id, rx, sentinel)
+        let (tx, rx) = tokio::sync::mpsc::channel(1);
+        let id = Id::generate();
+        let sentinel = Sentinel::new(id.clone(), tx);
+        (id, rx, sentinel)
     }
 
     fn sentinel_reify(state: State) -> (Id, Receiver<Message>, Sentinel) {
-	let (tx, rx) = tokio::sync::mpsc::channel(1);
-	let id = Id::generate();
-	let sentinel = Sentinel::reify(id.clone(), state, tx);
-	(id, rx, sentinel)
+        let (tx, rx) = tokio::sync::mpsc::channel(1);
+        let id = Id::generate();
+        let sentinel = Sentinel::reify(id.clone(), state, tx);
+        (id, rx, sentinel)
     }
 
     #[tokio::test]
     async fn valid_from_queued_to_start() {
-	let (id, mut rx, mut sentinel) = sentinel();
+        let (id, mut rx, mut sentinel) = sentinel();
 
-	let handle = tokio::spawn(async move {
-	    assert!(matches!(rx.recv().await.unwrap(), Message::UpdateOperation{
-		id,
-		from: State::Queued,
-		to: State::Working,
-	    }))
-	});
+        let handle = tokio::spawn(async move {
+            assert!(matches!(
+                rx.recv().await.unwrap(),
+                Message::UpdateOperation {
+                    id,
+                    from: State::Queued,
+                    to: State::Working,
+                }
+            ))
+        });
 
-	sentinel.start().await;
-	handle.await;
+        sentinel.start().await;
+        handle.await;
     }
 
     #[tokio::test]
     async fn valid_from_queued_to_cancel() {
-	let (id, mut rx, mut sentinel) = sentinel();
+        let (id, mut rx, mut sentinel) = sentinel();
 
-	let handle = tokio::spawn(async move {
-	    assert!(matches!(rx.recv().await.unwrap(), Message::UpdateOperation{
-		id,
-		from: State::Queued,
-		to: State::Canceled,
-	    }))
-	});
+        let handle = tokio::spawn(async move {
+            assert!(matches!(
+                rx.recv().await.unwrap(),
+                Message::UpdateOperation {
+                    id,
+                    from: State::Queued,
+                    to: State::Canceled,
+                }
+            ))
+        });
 
-	sentinel.cancel().await;
-	handle.await;
+        sentinel.cancel().await;
+        handle.await;
     }
 
     #[tokio::test]
     async fn invalid_from_queued_to_failed() {
-	let (id, mut rx, mut sentinel) = sentinel();
+        let (id, mut rx, mut sentinel) = sentinel();
 
-	assert!(matches!(
-	    sentinel.fail(OperationError::Sender).await,
-	    Err(OperationError::InvalidTransition {
-		from: State::Queued,
-		to: State::Failed
-	    }))
-	);
+        assert!(matches!(
+            sentinel.fail(OperationError::Sender).await,
+            Err(OperationError::InvalidTransition {
+                from: State::Queued,
+                to: State::Failed
+            })
+        ));
     }
 
     #[tokio::test]
     async fn invalid_from_queued_to_complete() {
-	let (id, mut rx, mut sentinel) = sentinel();
+        let (id, mut rx, mut sentinel) = sentinel();
 
-	assert!(matches!(
-	    sentinel.complete().await,
-	    Err(OperationError::InvalidTransition {
-		from: State::Queued,
-		to: State::Completed
-	    }))
-	);
+        assert!(matches!(
+            sentinel.complete().await,
+            Err(OperationError::InvalidTransition {
+                from: State::Queued,
+                to: State::Completed
+            })
+        ));
     }
 
     #[tokio::test]
     async fn valid_from_working_to_cancel() {
-	let (id, mut rx, mut sentinel) = sentinel();
+        let (id, mut rx, mut sentinel) = sentinel();
 
-	let handle = tokio::spawn(async move {
-	    assert!(matches!(rx.recv().await.unwrap(), Message::UpdateOperation{
-		id,
-		from: State::Queued,
-		to: State::Working,
-	    }));
+        let handle = tokio::spawn(async move {
+            assert!(matches!(
+                rx.recv().await.unwrap(),
+                Message::UpdateOperation {
+                    id,
+                    from: State::Queued,
+                    to: State::Working,
+                }
+            ));
 
-	    assert!(matches!(rx.recv().await.unwrap(), Message::UpdateOperation{
-		id,
-		from: State::Working,
-		to: State::Canceled,
-	    }))
-	});
+            assert!(matches!(
+                rx.recv().await.unwrap(),
+                Message::UpdateOperation {
+                    id,
+                    from: State::Working,
+                    to: State::Canceled,
+                }
+            ))
+        });
 
-	sentinel.start().await;
-	sentinel.cancel().await;
-	handle.await;
+        sentinel.start().await;
+        sentinel.cancel().await;
+        handle.await;
     }
 
     #[tokio::test]
     async fn valid_from_working_to_failed() {
-	let (id, mut rx, mut sentinel) = sentinel();
+        let (id, mut rx, mut sentinel) = sentinel();
 
-	let handle = tokio::spawn(async move {
-	    assert!(matches!(rx.recv().await.unwrap(), Message::UpdateOperation{
-		id,
-		from: State::Queued,
-		to: State::Working,
-	    }));
+        let handle = tokio::spawn(async move {
+            assert!(matches!(
+                rx.recv().await.unwrap(),
+                Message::UpdateOperation {
+                    id,
+                    from: State::Queued,
+                    to: State::Working,
+                }
+            ));
 
-	    assert!(matches!(rx.recv().await.unwrap(), Message::UpdateOperation{
-		id,
-		from: State::Working,
-		to: State::Failed,
-	    }))
-	});
+            assert!(matches!(
+                rx.recv().await.unwrap(),
+                Message::UpdateOperation {
+                    id,
+                    from: State::Working,
+                    to: State::Failed,
+                }
+            ))
+        });
 
-	sentinel.start().await;
-	sentinel.fail(OperationError::Sender).await;
-	handle.await;
+        sentinel.start().await;
+        sentinel.fail(OperationError::Sender).await;
+        handle.await;
     }
 
     #[tokio::test]
     async fn valid_from_working_to_complete() {
-	let (id, mut rx, mut sentinel) = sentinel();
+        let (id, mut rx, mut sentinel) = sentinel();
 
-	let handle = tokio::spawn(async move {
-	    assert!(matches!(rx.recv().await.unwrap(), Message::UpdateOperation{
-		id,
-		from: State::Queued,
-		to: State::Working,
-	    }));
+        let handle = tokio::spawn(async move {
+            assert!(matches!(
+                rx.recv().await.unwrap(),
+                Message::UpdateOperation {
+                    id,
+                    from: State::Queued,
+                    to: State::Working,
+                }
+            ));
 
-	    assert!(matches!(rx.recv().await.unwrap(), Message::UpdateOperation{
-		id,
-		from: State::Working,
-		to: State::Completed,
-	    }))
-	});
+            assert!(matches!(
+                rx.recv().await.unwrap(),
+                Message::UpdateOperation {
+                    id,
+                    from: State::Working,
+                    to: State::Completed,
+                }
+            ))
+        });
 
-	sentinel.start().await;
-	sentinel.complete().await;
-	handle.await;
+        sentinel.start().await;
+        sentinel.complete().await;
+        handle.await;
     }
 
     #[tokio::test]
     async fn invalid_from_complete_to_fail() {
-	let (id, mut rx, mut sentinel) = sentinel_reify(State::Completed);
+        let (id, mut rx, mut sentinel) = sentinel_reify(State::Completed);
 
-	assert!(matches!(
-	    sentinel.fail(OperationError::Sender).await,
-	    Err(OperationError::InvalidTransition {
-		from: State::Completed,
-		to: State::Failed
-	    }))
-	);
+        assert!(matches!(
+            sentinel.fail(OperationError::Sender).await,
+            Err(OperationError::InvalidTransition {
+                from: State::Completed,
+                to: State::Failed
+            })
+        ));
     }
 
     #[tokio::test]
     async fn invalid_from_complete_to_cancel() {
-	let (id, mut rx, mut sentinel) = sentinel_reify(State::Completed);
+        let (id, mut rx, mut sentinel) = sentinel_reify(State::Completed);
 
-	assert!(matches!(
-	    sentinel.cancel().await,
-	    Err(OperationError::InvalidTransition {
-		from: State::Completed,
-		to: State::Canceled
-	    }))
-	);
+        assert!(matches!(
+            sentinel.cancel().await,
+            Err(OperationError::InvalidTransition {
+                from: State::Completed,
+                to: State::Canceled
+            })
+        ));
     }
 
     #[tokio::test]
     async fn reify_with_initial_state() {
-	let (tx, rx) = tokio::sync::mpsc::channel(1);
-	let id = Id::generate();
-	let sentinel = Sentinel::reify(id.clone(), State::Failed, tx);
+        let (tx, rx) = tokio::sync::mpsc::channel(1);
+        let id = Id::generate();
+        let sentinel = Sentinel::reify(id.clone(), State::Failed, tx);
 
-	assert_eq!(State::Failed, sentinel.state());
+        assert_eq!(State::Failed, sentinel.state());
     }
 }
